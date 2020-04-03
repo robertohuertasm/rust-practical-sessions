cf80f1c7a088b4a2cc547717a0791f46b08df847
Roberto Huertas <roberto.huertas@outlook.com>
2020-04-03 03:24:15 +0200

    feat(datetime): added datetime support

--- 01-grpc-server/src/data.rs
+++ 01-grpc-server/src/data.rs
@@ -1,6 +1,11 @@
 use crate::proto::User;
 use anyhow::Result;
-use sqlx::PgPool;
+use prost_types::Timestamp;
+
+use sqlx::{
+    types::chrono::{DateTime, NaiveDate, Utc},
+    PgPool,
+};
 
 pub struct PostgresRepository {
     pub pool: PgPool,
@@ -15,9 +20,13 @@ impl PostgresRepository {
     pub async fn get_user(&self, name: &str) -> Result<User> {
         let pool = &self.pool;
 
-        let raw_user = sqlx::query_as!(RawUser, "SELECT id, name FROM users where name = $1", name)
-            .fetch_one(pool)
-            .await?;
+        let raw_user = sqlx::query_as!(
+            RawUser,
+            "SELECT id, name, birth_date, created_at, updated_at, custom_data FROM users where name = $1",
+            name
+        )
+        .fetch_one(pool)
+        .await?;
 
         println!("{:?}", raw_user);
 
@@ -29,6 +38,10 @@ impl PostgresRepository {
 struct RawUser {
     pub id: uuid::Uuid,
     pub name: String,
+    pub birth_date: Option<NaiveDate>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+    pub custom_data: serde_json::Value,
 }
 
 impl From<RawUser> for User {
@@ -36,9 +49,23 @@ impl From<RawUser> for User {
         Self {
             id: raw_user.id.to_string(),
             name: raw_user.name,
-            birth_date: None,
-            created_at: None,
-            updated_at: None,
+            birth_date: naive_to_timestamp(raw_user.birth_date),
+            created_at: to_timestamp(raw_user.created_at),
+            updated_at: to_timestamp(raw_user.updated_at),
         }
     }
+}
+
+fn to_timestamp(datetime: Option<DateTime<Utc>>) -> Option<Timestamp> {
+    datetime.map(|dt| Timestamp {
+        seconds: dt.timestamp(),
+        nanos: 0,
+    })
+}
+
+fn naive_to_timestamp(date: Option<NaiveDate>) -> Option<Timestamp> {
+    date.map(|d| Timestamp {
+        seconds: d.and_hms(0, 0, 0).timestamp(),
+        nanos: 0,
+    })
 }
